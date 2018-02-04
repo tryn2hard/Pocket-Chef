9873be30f100ae3af5d61cdf89efc6b061346171
Rob Quick <requick1031@gmail.com>
2018-02-04 14:04:31 +0900

    changed fragment_recipe.xml to have an default image

--- app/src/main/java/com/example/robot/pocket_chef/RecipeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/robot/pocket_chef/RecipeRecyclerViewAdapter.java
@@ -36,7 +36,6 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
         holder.mContentView.setText(mValues.get(position).content);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
@@ -58,15 +57,13 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
         public final TextView mContentView;
         public DummyItem mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
+                      mContentView = (TextView) view.findViewById(R.id.recipe_name);
         }
 
         @Override
