98e044789c3b6f8c8337eea4805d0a900cf0dc28
Rob Quick <requick1031@gmail.com>
2018-02-04 13:06:04 +0900

    attached generic fragment to RecipeActivity

--- app/src/main/java/com/example/robot/pocket_chef/RecipeActivity.java
+++ app/src/main/java/com/example/robot/pocket_chef/RecipeActivity.java
@@ -1,13 +1,32 @@
 package com.example.robot.pocket_chef;
 
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RecipeActivity extends AppCompatActivity {
+import com.example.robot.pocket_chef.dummy.DummyContent;
+
+public class RecipeActivity extends AppCompatActivity implements RecipeFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        Fragment recipeListFragment = fm.findFragmentById(R.id.recipe_fragment_container);
+
+        if(recipeListFragment == null){
+            recipeListFragment = new RecipeFragment();
+            fm.beginTransaction()
+                    .add(R.id.recipe_fragment_container, recipeListFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
